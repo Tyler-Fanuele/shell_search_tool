003e650897776428a92e6a7146aa841dbb547fe6
Tyler Fanuele <tfanuele@gmail.com>
2022-04-24 18:13:22 -0400

    DELETED TESTS

--- pdf.py
+++ pdf.py
@@ -7,11 +7,12 @@ def main():
     path = os.getcwd()
     files = [f for f in os.listdir(".") if os.path.isfile(f)]
     needed = []
+    unwanted = []
     for f in files:
         if re.search(r"\w(\.cpp)", f) or re.search(r"\w*(\.h)", f) or re.search(r"Makefile", f):
             print(f)
             os.system("enscript -C --margins=50:50:50:50 " + f + " -o " + f + ".ps &&  ps2pdf " + f + ".ps")
-            needed.append(f + ".ps")
+            unwanted.append(f + ".ps")
             needed.append(f + ".pdf")
     print("Making pdf directory")
     os.system("mkdir pdf")
@@ -19,6 +20,9 @@ def main():
     for f in needed:
         os.system("cp " + f + " pdf/" + f)
         os.system("rm " + f)
+    for f in unwanted:
+        os.system("rm " + f)
+
     
     
             
